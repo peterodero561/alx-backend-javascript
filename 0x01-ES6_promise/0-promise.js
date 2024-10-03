553b02fce1326d0eab29a321f56570d671380f51
Peter Odero <peterodero561@gmail.com>
2024-10-03 09:32:10 +0300

    fixing lint issues

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,13 +1,13 @@
 function getResponseFromAPI() {
-	return new Promise((resolve, reject) => {
-		const success = true;
+  return new Promise((resolve, reject) => {
+    const success = true;
 
-		if (success) {
-			resolve('API call successful');
-		} else {
-			reject('API call failed');
-		}
-	});
+    if (success) {
+      resolve('API call successful');
+    } else {
+      reject(new Error('API call failed'));
+    }
+  });
 }
 
 export default getResponseFromAPI;
