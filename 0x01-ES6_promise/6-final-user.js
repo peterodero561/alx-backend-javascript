26949b0d9268e6a38853c335a1e90325e21b58f2
Peter Odero <peterodero561@gmail.com>
2024-10-03 10:25:01 +0300

    fix: 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,12 @@
-import signUpUser from './4-user-promise.js'
-import uploadPhoto from './5-photo-reject.js'
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstname, lastname, filename) {
-	return Promise.allSettled([signUpUser(firstname, lastname), uploadPhoto(filename)])
-	.then((results) => {
-		return results.map((result) => {
-			if (result.status === 'fulfilled') {
-				return { 'status': result.status, 'value': result.value };
-			} else {
-				return { 'status': result.status, 'value': result.reason };
-			}
-		});
-	});
+  return Promise.allSettled([signUpUser(firstname, lastname), uploadPhoto(filename)])
+    .then((results) => results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return { status: result.status, value: result.value };
+      }
+      return { status: result.status, value: result.reason };
+    }));
 }
