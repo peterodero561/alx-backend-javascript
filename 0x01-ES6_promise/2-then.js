05330c9a8da0475363c5b7a33880d13d981d89ad
Peter Odero <peterodero561@gmail.com>
2024-10-03 09:46:16 +0300

    update

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,12 @@
 function handleResponseFromAPI(promise) {
-	return promise
-		.then(() => {
-			console.log("Got a responce from API");
-			return {
-				'status': 200,
-				'body': 'success'
-			};
-		})
-		.catch(() => {
-			return new Error();
-		});
+  return promise
+    .then(() => {
+      console.log('Got a responce from API');
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch(() => new Error());
 }
 export default handleResponseFromAPI;
