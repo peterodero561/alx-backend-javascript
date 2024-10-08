cabbb348e00a3787e9caffc0545fbdfee81a0291
Peter Odero <peterodero561@gmail.com>
2024-10-08 14:55:08 +0300

    build: Create a function named groceriesList that returns a map of groceries

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const groce = new Map();
+  groce.set('Apples', 10);
+  groce.set('Tomatoes', 10);
+  groce.set('Pasta', 1);
+  groce.set('Rice', 1);
+  groce.set('Banana', 5);
+
+  return groce;
+}
