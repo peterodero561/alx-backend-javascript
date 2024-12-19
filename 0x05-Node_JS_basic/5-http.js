68b96c226a8d7c3f3ae222a48c68ea610766682e
Peter Odero <peterodero561@gmail.com>
2024-12-19 12:24:06 +0300

    build: created a more complex server using http module

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,34 @@
+const http = require('http')
+const countStudents = require('./3-read_file_async');
+// A more complex server
+const app = http.createServer((req, res) => {
+	const { url } = req;
+
+	if (url === '/') {
+		res.writeHead(200, { 'Content-Type': 'text/plain' });
+		 res.end('Hello Holberton School');
+	} else if (url.startsWith('/students')) {
+		const databasePath = process.argv[2];
+
+		res.writeHead(200, { 'Content-Type': 'text/plain' });
+		res.write('This is the list of our students\n');
+
+		countStudents(databasePath)
+		.then((result) => {
+			res.write(result);
+			res.end();
+		})
+		.catch((err) => {
+			res.end(err.message);
+		});
+	} else {
+		res.writeHead(404, { 'Content-Type': 'text/plain' });
+		res.end('Not Found')
+	}
+});
+
+app.listen(1245, () => {
+	console.log('Server is listening to port 1245');
+});
+
+module.exports = app;
