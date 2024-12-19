1c4802ffd7f15deb0bc9bde502f771a7a53e4ce3
Peter Odero <peterodero561@gmail.com>
2024-12-19 11:42:43 +0300

    build: created a small HTTP server using http module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+// create http server
+const app = http.createServer((req, res) => {
+  res.statusCode = 200; // Setting statusCode
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello ALX!');
+});
+
+app.listen(1245, () => {
+  console.log('Server is listening on port 1245');
+});
+
+module.exports = app;
