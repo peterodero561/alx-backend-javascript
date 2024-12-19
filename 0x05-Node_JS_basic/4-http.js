8bcd1439e2ad5e9a706a72cfd3898ee2c3005a75
Peter Odero <peterodero561@gmail.com>
2024-12-19 11:49:04 +0300

    fix: changed the response message

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,7 +4,7 @@ const http = require('http');
 const app = http.createServer((req, res) => {
   res.statusCode = 200; // Setting statusCode
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello ALX!');
+  res.end('Hello Holberton School!');
 });
 
 app.listen(1245, () => {
